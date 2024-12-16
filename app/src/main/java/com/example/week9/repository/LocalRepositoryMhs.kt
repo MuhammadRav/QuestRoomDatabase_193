84f882e913308b77bc236e691f0d3d4de7fbf7e2
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-16 21:59:05 +0700

    menambahkan fungsi crud

--- app/src/main/java/com/example/week9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/week9/repository/LocalRepositoryMhs.kt
@@ -2,10 +2,19 @@ package com.example.week9.repository
 
 import com.example.week9.data.dao.MahasiswaDao
 import com.example.week9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ): RepositoryMhs {
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
--- app/src/main/java/com/example/week9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/week9/repository/RepositoryMhs.kt
@@ -1,8 +1,13 @@
 package com.example.week9.repository
 
 import com.example.week9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
 
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
     suspend fun insertMhs(mahasiswa: Mahasiswa)
 }
