5e919c6df66fa7a10b9fc17ee9d2d30c2b3e6b79
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2024-12-16 21:08:24 +0700

    menambahkan HomeMhsViewModel untuk mengelola state UI pada layar Home mahasiswa

---
+++ app/src/main/java/com/example/week9/ui/viewModel/HomeMhsViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.week9.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.week9.data.entity.Mahasiswa
+import com.example.week9.repository.RepositoryMhs
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+
+data class  HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoader: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
